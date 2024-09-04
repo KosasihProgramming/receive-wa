cacdcb92335636730f44b6bcdf20cf97181c0309
KosasihProgramming <growthhuman7@gmail.com>
2024-09-04 08:35:06 +0700

    ukbh

--- src/Routes/TelukRoute.js
+++ src/Routes/TelukRoute.js
@@ -7,3 +7,18 @@ router.get("/teluk/transaksi", getTransaksi);
 router.get("/teluk/send", sendWa);
 
 module.exports = router;
+
+
+
+// if (
+//     dataReceive.is_from_me == false &&
+//     dataReceive.name !== "status" &&
+//     dataReceive.message_body !== "" &&
+//     dataReceive.message_body !== undefined &&
+//     dataReceive.message_body == "A" &&
+//     (dataReceive.message_body == "A." ||
+//       dataReceive.message_body == "A. Puas" ||
+//       dataReceive.message_body == "A.Puas" ||
+//       dataReceive.message_body.includes("Puas") ||
+//       dataReceive.message_body.includes("puas"))
+//   ) {
